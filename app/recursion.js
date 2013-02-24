0bfb28c776129ce26ac57d958fe1f1a13db9bc00
Trevor Lalish-Menagh <trev@trevreport.org>
2013-02-24 17:50:04 -0500

    All done

--- app/recursion.js
+++ app/recursion.js
@@ -42,7 +42,55 @@ define(function() {
     },
 
     permute: function(arr) {
+      var i, len = arr.length, work, prev = arr, result = [arr];
 
+      function next(a) {
+        var i, j, k = -1, l = -1, len = a.length, tmp, end = false;
+
+        for (i = len - 2, j = len - 1; i >= 0; i -= 1, j -= 1) {
+          if (a[i] < a[j] && i > k) {
+            k = i;
+          }
+        }
+        if (k === -1) {
+          end = true;
+        } else {
+          for (i = 0; i < len; i += 1) {
+            if (a[k] < a[i] && i > l) {
+              l = i;
+            }
+          }
+          if (l === -1) {
+            end = true;
+          } else {
+            tmp = a[k];
+            a[k] = a[l];
+            a[l] = tmp;
+            for (i = k + 1, j = len - 1; i <= j; i += 1, j -= 1) {
+              tmp = a[i];
+              a[i] = a[j];
+              a[j] = tmp;
+            }
+          }
+        }
+
+        return end ? false : a;
+      }
+
+      do {
+        work = new Array(4);
+
+        for (i = 0; i < len; i += 1) {
+          work[i] = prev[i];
+        }
+
+        result.push(work);
+        work = next(work);
+        prev = work;
+      } while (work);
+
+      result.pop();
+      return result;
     }
   };
 });
